91b459959b5a6f1de31ba6a1d16070f2b94b5c8d
antonprins2001 <cdb364@alumni.ku.dk>
2026-04-16 13:31:40 +0200

    Add files via upload

---
+++ experiment.py
@@ -0,0 +1,6 @@
+from psychopy import visual, core, event
+from settings import Settings
+from participant import Participant
+from block import Block
+from trial import Trial
+from data_manager import DataManager
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    FULLSCREEN = False
+    WINDOW_SIZE = (1200, 800)
+    BG_COLOR = "black"
+    TEXT_COLOR = "white"
+
+    BREAK_TIME = 0.5
+    STIMULUS_TIME = 1.0
+
+    RESPONSE_KEYS = ["left", "right"]
+    N_BLOCKS = 2
