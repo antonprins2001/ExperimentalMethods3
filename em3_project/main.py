d956b521f8f4c82d57f5a55486f06bfa5923269a
Vinterblomsten <christoffer@kassoejuhl.dk>
2026-05-07 11:25:41 +0200

    new_seq

--- em3_project/main.py
+++ em3_project/main.py
@@ -909,7 +909,7 @@ def CollectTrials(trial_seqs, subject_id):
 
     return test_data, trial_data
 
-path = "Sequence/sequences.csv"
+path = "Sequence/sequences_new.csv"
 practice_path = 'Sequence/practice_sequences.csv'
 
 trial_seqs = GenerateTrials(path)
